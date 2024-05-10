866a3ec7d794337c3614a06be3c696231495e4be
supertick <grog@myrobotlab.org>
2024-05-10 10:44:08 -0700

    initial checkin

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,34 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "matlabupload": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "matlabuploadbf2cde1b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Triggeraf278fba": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "matlabuploads3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { Amplify } from 'aws-amplify';
+import { uploadData } from 'aws-amplify/storage';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/system';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+const StyledInput = styled('input')({
+  display: 'none', // Hide the actual input
+});
+
+function App({ signOut, user }) {
+  const [fileData, setFileData] = useState(null);
+  const [fileStatus, setFileStatus] = useState(false);
+
+  const uploadFile = async () => {
+    if (!fileData) return; // Ensure there is file data to upload
+
+    const filePath = `public/album/${new Date().getFullYear()}/${fileData.name}`;
+
+    try {
+      const result = await uploadData({
+        path: filePath,
+        data: fileData
+      }).result;
+      console.log('Succeeded: ', result);
+      setFileStatus(true);
+    } catch (error) {
+      console.log('Error : ', error);
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#b3e5fc' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>MatLab Upload App</h1>
+        <h3>Hello {user.username}</h3>
+        <Button variant="contained" color="primary" onClick={signOut}>
+          Sign out
+        </Button>
+        <br />
+        <br />
+        <label htmlFor="contained-button-file">
+          <StyledInput
+            accept="image/*"
+            id="contained-button-file"
+            multiple
+            type="file"
+            onChange={(e) => {
+              setFileData(e.target.files[0]);
+              setFileStatus(false); // Reset upload status on new file selection
+            }}
+          />
+          <Button variant="contained" component="span">
+            Choose File
+          </Button>
+        </label>
+        {fileData && (
+          <div>
+            <Button variant="contained" color="secondary" onClick={uploadFile}>
+              Upload File
+            </Button>
+          </div>
+        )}
+        {fileStatus && <p>File uploaded successfully</p>}
+      </div>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
